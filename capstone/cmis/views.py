d19d3c9b73b751476d1fc6aaaf108707d10820cd
spyke1215 <insyde.dedsec@gmail.com>
2022-07-05 09:13:33 +0800

    mapbox added test

--- capstone/cmis/views.py
+++ capstone/cmis/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,'cmis/index.html')
+
+def menu (request):
+    return render(request,'cmis/menu.html')
