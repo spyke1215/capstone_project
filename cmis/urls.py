1f798fa1b2f751d95a68f44ba81d3b355116a756
spyke1215 <insyde.dedsec@gmail.com>
2022-10-11 00:28:27 +0800

    added search(unfinished) and deceased functions

--- cmis/urls.py
+++ cmis/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("menu", views.menu, name="menu"),
     path("cemetery", views.cemetery, name="cemetery"),
-    path("deceased", views.deceased, name="info"),
+    path("deceased", views.deceased, name="deceased"),
     path("search", views.search, name="search"),
 ]
--- cmis/views.py
+++ cmis/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from cmis.models import *
+from django.db.models import Q
+
 
 # Create your views here.
-def index(request):
-    return render(request,'cmis/index.html')
+def menu(request):
+    return render(request,'cmis/menu.html')
 
 def cemetery(request):
     return render(request,'cmis/cemetery.html')
 
 def search(request):
+    if request.method == "POST":
 
+        first = request.POST.get("first")
+        middle = request.POST.get("middle")
+        last = request.POST.get("last")
+        birth = request.POST.get("birth")
+        death = request.POST.get("death")
+
+        return render(request,'cmis/search.html',{
+            "deceased": Deceased.objects.filter(Q(first_name__iexact=first) | Q(middle_name__iexact=middle) 
+            | Q(last_name__iexact=last))
+        })
+    
     return render(request,'cmis/search.html',{
-        "deceased": Deceased.objects.all()
-    })
-
+            "deceased": Deceased.objects.all()
+        })
 def deceased(request):
     return render(request,'cmis/deceased.html', {
-        "deceased": Deceased.objects.get(pk=1)
+        "deceased": Deceased.objects.get(pk=7)
     })
