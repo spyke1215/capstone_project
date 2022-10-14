1cc83c1c53d4d8241744e8b6e37d20c14ca1ae78
spyke1215 <insyde.dedsec@gmail.com>
2022-10-14 12:34:16 +0800

    heroku settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
