a21762f74b79eb4987682125e7ef3cbd179054ab
Restyled.io <commits@restyled.io>
2022-11-23 21:00:49 +0000

    Restyled by clang-format

--- cmis/static/cmis/js/scripts.js
+++ cmis/static/cmis/js/scripts.js
@@ -1,51 +1,50 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Grayscale v7.0.5
+ * (https://startbootstrap.com/theme/grayscale) Copyright 2013-2022 Start
+ * Bootstrap Licensed under MIT
+ * (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
-
-    if (window.location.pathname == '/') {
-        document.getElementById("scrolldown").addEventListener("click", function(){
-            document.getElementById("cemeterySelect").scrollIntoView({behavior: "smooth"});
-        });
-    }
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  if (window.location.pathname == '/') {
+    document.getElementById("scrolldown").addEventListener("click", function() {
+      document.getElementById("cemeterySelect").scrollIntoView({
+        behavior : "smooth"
+      });
     });
-});
+  }
 
+  // Navbar shrink function
+  var navbarShrink = function() {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink')
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems =
+      [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+  responsiveNavItems.map(function(responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
+});
